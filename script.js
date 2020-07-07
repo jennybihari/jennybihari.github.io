fd54f9027e8e476152077102de08ca093fb0e716
jennybihari <64094994+jennybihari@users.noreply.github.com>
2020-07-07 19:39:32 +0200

    lines

---
+++ script.js
@@ -0,0 +1,13 @@
+//nav
+
+$('.burgerIcon').hover
+
+    (function(){
+        TweenMax.to('.burgerLine:first-child', 0.2, {x: -10});
+        TweenMax.to('.burgerLine:last-child', 0.2, {x: 10});
+    },
+
+    function(){
+        TweenMax.to('.burgerLine:first-child', 0.2, {x: -10});
+        TweenMax.to('.burgerLine:last-child', 0.2, {x: 10});
+    });
